cd013b06b04bc35233a1b5c2f7b837429e032bbc
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-11-23 16:04:44 +0100

    add section + modal

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((i) => i.addEventListener("click", linkAction));
+
+/*=============== SHADOW HEADER ===============*/
+
+/*=============== EMAIL JS ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
