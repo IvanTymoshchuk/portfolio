6ade3f31b8fccc0f80a218cc37eb8fe70290e19f
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-11-29 15:31:46 +0100

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,14 @@ navLink.forEach((i) => i.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
 
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
+
 /*=============== EMAIL JS ===============*/
 
 /*=============== SHOW SCROLL UP ===============*/
