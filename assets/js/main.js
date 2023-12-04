72ecece80670669698ca2d8aec436bb9e38e2fed
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-12-04 16:07:11 +0100

    dart-theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,8 +78,39 @@ const scrollUp = () => {
     : scrollUp.classList.remove("show-scroll");
 };
 window.addEventListener("scroll", scrollUp);
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== DARK LIGHT THEME ===============*/
+
+const themeButton = document.getElementById("theme-btn");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+
+const getCurrentIcon = () =>
+  document.body.classList.contains(iconTheme)
+    ? "ri-moon-clear-line"
+    : "ri-sun-line";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[
+    selectedIcon === "ri-moon-clear-line" ? "add" : "remove"
+  ];
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.body.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
