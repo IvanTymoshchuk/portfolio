87eefde7351ebeedf6deb83e5e39a07f3df01088
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-12-04 14:58:41 +0100

    js-conact

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,37 @@ const shadowHeader = () => {
 window.addEventListener("scroll", shadowHeader);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
 
+const sendEmail = (e) => {
+  e.preventDefault();
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_g4wah68",
+      "template_omz031u",
+      "#contact-form",
+      "P8j-HPgeqVa3ti0oj"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 3000);
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+contactForm.addEventListener("submit", sendEmail);
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
